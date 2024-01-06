6649a1f7502ba68e8b66f7ae9a838e50c00e9fe2
Marcial Igme <88986288+igmeMarcial@users.noreply.github.com>
2024-01-06 13:53:46 -0500

    add navbar

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
+    
     <h1>Pagina DE Chaparra</h1>
   )
 }
---
+++ constants/index.ts
@@ -0,0 +1,9 @@
+// NAVIGATION
+export const NAV_LINKS = [
+    { href: '/', key: 'inicio', label: 'Inicio' },
+    { href: '/', key: 'municipalidad', label: 'Municipalidad' },
+    { href: '/', key: 'tramites', label: 'Tramites' },
+    { href: '/', key: 'obras', label: 'Obras ' },
+    { href: '/', key: 'historia', label: 'Historia' },
+    { href: '/', key: 'transparencia', label: 'Tranparencia' },
+  ];
